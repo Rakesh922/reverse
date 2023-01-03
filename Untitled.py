0de3613396a3f91cf4a656b88bc38b3eb443cc7e
Rakesh922 <102220582+Rakesh922@users.noreply.github.com>
2023-01-03 15:53:45 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import reverse
+
+reverse.mirror()
+# In[2]:
+
+
+reverse.mirror()
+
+
+# In[ ]:
+
+
+
+
---
+++ reverse.py
@@ -0,0 +1,7 @@
+def mirror():
+    word = input(" ")
+    for char in range(len(word) - 1, -1, -1):
+        print(word[char], end="")
+        
+       
+        
